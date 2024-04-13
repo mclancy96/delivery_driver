51ac398aa2cd98b7c39fd8638d91c7fc3c986104
Mike Clancy <mclancy96@gmail.com>
2024-04-13 16:21:53 -0400

    Driver working

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    [SerializeField] float rotateSpeed = 300f;
+    [SerializeField] float translateSpeed = 20f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float rotateAmount = Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime;
+        float translateAmount = Input.GetAxis("Vertical") * translateSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, -rotateAmount);
+        transform.Translate(0.0f, translateAmount, 0.0f);
     }
 }
